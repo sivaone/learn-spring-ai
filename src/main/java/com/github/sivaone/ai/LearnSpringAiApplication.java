6c8505406e1cf5ec21bf384d16ec4e48f6d8a155
Siva Nagi Reddy <siva.alla@outlook.com>
2026-04-20 21:36:58 -0500

    Initial commit

---
+++ src/main/java/com/github/sivaone/ai/LearnSpringAiApplication.java
@@ -0,0 +1,12 @@
+package com.github.sivaone.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearnSpringAiApplication.class, args);
+  }
+}
---
+++ src/test/java/com/github/sivaone/ai/LearnSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sivaone.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class LearnSpringAiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
